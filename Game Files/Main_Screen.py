5c9a3bafd99fcd9de053415855165c8be4e91c6b
Luiz Hosfeld <Lh15s@my.fsu.edu>
2018-11-08 15:42:19 -0500

    Testing Buttons and interactions with the mouse

--- Game Files/Main_Screen.py
+++ Game Files/Main_Screen.py
@@ -12,9 +12,7 @@ background = pygame.Surface(gameDisplay.get_size())
 background.fill(bgcolor)
 
 background = background.convert()
-
-pygame.draw.rect(background, (255,255,255), (530,5,300,150))
-
+pygame.draw.rect(background, (255, 255, 255), (530, 5, 300, 150))  # Coordinate: 530 = x, 5 = y, 300 width, 150 = height
 gameDisplay.blit(background, (0, 0))
 
 pygame.display.set_caption('F.U.N.')
@@ -24,10 +22,26 @@ pygame.display.init()
 playing = True
 
 while playing:
+    mouse_loc = pygame.mouse.get_pos()
     for _ in pygame.event.get():
         if _.type == pygame.QUIT:
             playing = False
             break
+        if _.type == pygame.MOUSEBUTTONDOWN:
+            mouse_loc = pygame.mouse.get_pos()
+            if 830 > mouse_loc[0] > 530 and 155 > mouse_loc[1] > 5:
+                pygame.draw.rect(background, (0, 0, 245), (20, 170, 10, 100))
+                pygame.draw.rect(background, (0, 0, 245), (30, 170, 30, 10))
+                pygame.draw.rect(background, (0, 0, 245), (30, 190, 30, 10))
+                gameDisplay.blit(background, (0, 0))
+    if 830 > mouse_loc[0] > 530 and 155 > mouse_loc[1] > 5:
+        pygame.draw.rect(background, (255, 0, 0), (530, 5, 300, 150))
+        gameDisplay.blit(background, (0, 0))
+    else:
+        pygame.draw.rect(background, (255, 255, 255), (530, 5, 300, 150))
+        gameDisplay.blit(background, (0, 0))
+
+
 
     pygame.display.flip()
 
