88cdac51f083586c239a88558a884842091269dc
Luiz Hosfeld <Lh15s@my.fsu.edu>
2018-11-08 14:10:41 -0500

    moved basic display

--- Game Files/Home.py
+++ Game Files/Home.py
@@ -1,5 +1,15 @@
 import pygame
 
+size = width, height = 1360, 768
 
-class Home(object):
-    pass
+pygame.display.set_mode(size)
+
+pygame.display.init()
+
+playing = True
+
+while playing:
+    for _ in pygame.event.get():
+        if _.type == pygame.QUIT:
+            playing = False
+            break
