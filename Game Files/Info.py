2c4449a3aa58f65a0fac69e1157596c4bc0c45ec
Nic Feanny <feannyn@gmail.com>
2018-11-15 10:24:26 -0500

    Completed template for Fighter info and Selection Screen

--- Game Files/Info.py
+++ Game Files/Info.py
@@ -6,20 +6,21 @@ purple = (150, 0, 150)
 white = (255, 255, 255)
 background_image = pygame.image.load('/Users/nicholasfeanny/Documents/Python/group_Project/F.U.N./Character Images/saltySpitoon.jpg')
 
-gameDisplay = pygame.display.set_mode((800, 600))
-#gameDisplay.fill(white);
+gameDisplay = pygame.display.set_mode((850, 600))
 gameDisplay.blit(background_image, (0, 0))
 
+pygame.draw.polygon(gameDisplay, white, [(450, 375), (500, 450), (600, 450), (650, 375), (600, 300), (500, 300)])
+pygame.draw.polygon(gameDisplay, purple, [(150, 375), (200, 450), (300, 450), (350, 375), (300, 300), (200, 300)])
+pygame.draw.polygon(gameDisplay, (15,139,141), [(300, 450), (350, 525), (450, 525), (500, 450), (450, 375), (350, 375)])
+pygame.draw.polygon(gameDisplay, purple, [(150, 525), (200, 600), (300, 600), (350, 525), (300, 450), (200, 450)])
+pygame.draw.polygon(gameDisplay, white , [(450, 525), (500, 600), (600, 600), (650, 525), (600, 450), (500, 450)])
+pygame.draw.polygon(gameDisplay, purple, [(600, 450), (650, 525), (750, 525), (795, 450), (750, 375), (650, 375)])
+pygame.draw.polygon(gameDisplay, (15,139,141), [(5, 450), (50, 525), (150, 525), (200, 450), (150, 375), (50, 375)])
 
-#pygame.draw.polygon(gameDisplay, purple, [(100, 100), (150, 150), (250, 150), (300, 100), (250, 50), (150, 50)])
-pygame.draw.polygon(gameDisplay, purple, [(400, 300), (450, 375), (550, 375), (600, 300), (550, 225), (450, 225)])
-pygame.draw.polygon(gameDisplay, (15,139,141), [(100, 300), (150, 375), (250, 375), (300, 300), (250, 225), (150, 225)])
-pygame.draw.polygon(gameDisplay, (236,154,41), [(250, 375), (300, 450), (400, 450), (450, 375), (400, 300), (300, 300)])
-pygame.draw.polygon(gameDisplay, (168,32,26), [(550, 375), (600, 450), (700, 450), (750, 375), (700, 300), (600, 300)])
-pygame.draw.polygon(gameDisplay, (168,32,26), [(250, 225), (300, 300), (400, 300), (450, 225), (400, 150), (300, 150)])
-pygame.draw.polygon(gameDisplay, (15,139,141), [(550, 225), (600, 300), (700, 300), (750, 225), (700, 150), (600, 150)])
+
 pygame.draw.rect(gameDisplay, purple, (10, 10, 30, 30))
-
+pygame.draw.rect(gameDisplay, white, (50, 50, 300, 200))
+pygame.draw.rect(gameDisplay, white, (450, 50, 300, 200))
 
 
 
